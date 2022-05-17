051137228c11c517de6ede32c9f0a982ef9a3119
lewski <leevia00@gmail.com>
2022-05-17 17:06:38 +0300

    made rng namespace way safer

--- src/rng.hpp
+++ src/rng.hpp
@@ -4,7 +4,7 @@
 
 
 namespace rng {
-  std::mt19937 rng{ std::random_device{}() };
-  std::uniform_int_distribution acc_roll{ 1, 100 };
-  std::uniform_int_distribution damage_roll{ 0, 4 };
+  static std::mt19937 rng{ std::random_device{}() };
+  static const std::uniform_int_distribution acc_roll{ 1, 100 };
+  static const std::uniform_int_distribution damage_roll{ 0, 4 };
 }
