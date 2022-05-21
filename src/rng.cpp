05555f8e581c7e637e459aaf4c24a08acde2a4e7
lewski <leevia00@gmail.com>
2022-05-21 11:44:03 +0300

    cleaned up rng implementation details

--- src/rng.cpp
+++ src/rng.cpp
@@ -6,11 +6,11 @@
 namespace rng {
   namespace {
     std::random_device rd;
-    std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
-    static std::mt19937 rng{ ss };
+    const std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+    std::mt19937 rng{ ss };
 
-    static const std::uniform_int_distribution acc_roll{ 1, 100 };
-    static const std::uniform_int_distribution damage_roll{ 0, 4 };
+    const std::uniform_int_distribution acc_roll{ 1, 100 };
+    const std::uniform_int_distribution damage_roll{ 0, 4 };
   }
 
   int AccRoll(int acc) {
