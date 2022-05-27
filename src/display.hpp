dd9321766a0d4de4e33c0798ea00d5e83d1a71d4
lewski <leevia00@gmail.com>
2022-05-27 16:30:57 +0300

    apparently inline fits better than static for this stuff?

--- src/display.hpp
+++ src/display.hpp
@@ -10,37 +10,37 @@ namespace display {
 
   void Stats(const std::vector<Wizard>& players);
 
-  static void Cast(const Wizard& w, const Card& card)
+  inline void Cast(const Wizard& w, const Card& card)
   {
     std::cout << w.name() << " casts " << card.name << "!\n";
   }
-  static void Fizzle(const Wizard& w)
+  inline void Fizzle(const Wizard& w)
   {
     std::cout << w.name() << " fizzles!\n";
   }
-  static void Pass(const Wizard& w)
+  inline void Pass(const Wizard& w)
   {
     std::cout << w.name() << " passes.\n";
   }
 
-  static void Damage(const Wizard& w, int damage)
+  inline void Damage(const Wizard& w, int damage)
   {
     std::cout << w.name() << " takes " << damage << " damage!\n";
   }
-  static void Healing(const Wizard& w, int heal)
+  inline void Healing(const Wizard& w, int heal)
   {
     std::cout << w.name() << " recovers " << heal << " hp!\n";
   }
-  static void Defeat(const Wizard& w)
+  inline void Defeat(const Wizard& w)
   {
     std::cout << "\n" << w.name() << " has been defeated!\n";
   }
-  static void Revive(const Wizard& w)
+  inline void Revive(const Wizard& w)
   {
     std::cout << w.name() << " has been revived!\n";
   }
 
-  static void UsedCharmOrWard(const HangingEffect& effect)
+  inline void UsedCharmOrWard(const HangingEffect& effect)
   {
     std::cout << SignedNumString(effect.strength) << "% "
       << effect.domain << "\n";
