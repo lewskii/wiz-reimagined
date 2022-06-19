3b52bf0e7bc5798c20430af87602cff23ae887d1
Leevi Aaltonen <leevia00@gmail.com>
2022-06-19 14:13:19 +0300

    changed how pips are printed

--- src/display.cpp
+++ src/display.cpp
@@ -2,18 +2,27 @@
 
 #include "display.hpp"
 
+static void PrintPips(int n, bool power = false)
+{
+  std::string label = power ? " power pip" : " pip";
+  if (n > 1 || n == 0)
+    label += 's';
+  std::cout << ", " << n << label;
+}
+
 
 void display::Stats(const std::vector<Wizard>& players)
 {
   std::cout << '\n';
   for (size_t i = 0; i < players.size(); ++i) {
     Wizard w = players[i];
-    std::cout << w.name() << ": "
-      << w.health() << " hp, "
-      << w.pips() << " pip(s)";
+    std::cout << w.name() << ": " << w.health() << " hp";
+
+    if (w.pips() > 0 || w.power_pips() == 0)
+      PrintPips(w.pips());
 
     if (w.power_pips() > 0)
-      std::cout << ", " << w.power_pips() << " power pip(s)";
+      PrintPips(w.power_pips(), true);
 
     std::cout << '\n';
   }
