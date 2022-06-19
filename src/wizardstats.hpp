5823ec64723909c4214b9e80f314fa6c15ae05f5
Leevi Aaltonen <leevia00@gmail.com>
2022-06-19 11:24:31 +0300

    wizards have schools

--- src/wizardstats.hpp
+++ src/wizardstats.hpp
@@ -4,14 +4,16 @@
 
 struct WizardStats {
 public:
-  WizardStats(const std::string name, const int hp)
+  WizardStats(std::string name, School school, int hp)
     :
     name{ name },
+    school{ school },
     max_health{ hp },
     health{ hp }
   {}
 
   const std::string name;
+  const School school;
   int max_health;
   int health;
 
