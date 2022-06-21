4b8de17e6615aaf5ae37d1559c1d32751de9adce
Leevi Aaltonen <leevia00@gmail.com>
2022-06-21 18:48:10 +0300

    added a function to randomise the order of two inputs

--- src/rng.hpp
+++ src/rng.hpp
@@ -9,4 +9,13 @@ namespace rng {
   int DamageRoll(int step);
   bool CoinToss();
 
+  template <typename T>
+  std::pair<T, T> RandomOrder(T first, T second)
+  {
+    if (CoinToss())
+      return std::make_pair(first, second);
+    else
+      return std::make_pair(second, first);
+  }
+
 }
